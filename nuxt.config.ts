1d1ced4a6d310569336a105be029fe8fd184fa74
Bxota <thomas.pro.leterme@gmail.com>
2025-04-27 19:46:59 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'],
-  
+  generate: {
+    subFolders: true,
+  },
   // Configuration app spécifique pour GitHub Pages
   app: {
     baseURL: '/',
