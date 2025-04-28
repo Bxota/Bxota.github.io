6500fa338b266b4003c172fd36d938b4e50d2614
Bxota <thomas.pro.leterme@gmail.com>
2025-04-28 15:29:34 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,70 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
-  target: 'static',
-  generate: {
-    dir: 'dist',
-    fallback: true
+  compatibilityDate: '2024-11-01',
+
+  devtools: { enabled: true },
+
+  css: ['@/assets/css/main.css'],
+
+  router: {
+    options: {
+      hashMode: true
+    }
   },
+
+  experimental: {
+    payloadExtraction: false
+  },
+
   app: {
-    // Ici, c'est très important : baseURL = /nom-de-ton-repo/
-    baseURL: '/Bxota.github.io',
-    buildAssetsDir: 'assets', // pas de / devant
+    baseURL: '/',
+    buildAssetsDir: 'assets',
     head: {
       link: [
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'
         }
+      ],
+      meta: [
+        {
+          'http-equiv': 'Permissions-Policy',
+          content: 'interest-cohort=(), browsing-topics=()'
+        }
       ]
     }
+  },
+
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'
+      }
+    ]
+  },
+
+  build: {
+    transpile: ['vue-remix-icons'],
   },
 
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/sitemap',
-    '@nuxtjs/robots'
+    '@nuxtjs/robots',
+    '@nuxtjs/google-fonts'
   ],
+  googleFonts: {
+    families: {
+      Montserrat: [300, 400, 600]
+    },
+    display: 'swap',
+    // S'assurer que les polices sont téléchargées et incluses dans le build
+    download: true,
+    // Inclure les polices localement dans le build
+    fontsDir: 'assets/fonts',
+    fontsPath: 'fonts'
+  },
 
   sitemap: {
     siteUrl: 'https://bxota.github.io',
@@ -37,20 +78,23 @@ export default defineNuxtConfig({
     robotsTxt: false
   },
 
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: [
-        '/', 
-        '/work',
-        '/certificates'
-      ]
+  "compilerOptions": {
+    "paths": {
+      "~/*": ["./*"],
+      "@/*": ["./*"]
     }
   },
 
-  build: {
-    transpile: ['vue-remix-icons'],
-  },
-
-  compatibilityDate: '2025-04-27'
+  nitro: {
+    preset: 'static',
+    prerender: {
+      crawlLinks: true,
+      routes: [
+        '/',
+        '/work',
+        '/certificates',
+        '/about',
+      ]
+    }
+  }
 })
